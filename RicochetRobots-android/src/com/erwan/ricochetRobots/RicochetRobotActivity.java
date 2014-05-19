e2d6b78206c530169ba74477a8a039114a7ddd0f
Theblues <erwan76.lebrun@gmail.com>
2014-05-20 00:03:08 +0200

    0.1.6.9 : On connect les deux appreils ensemble

--- RicochetRobots-android/src/com/erwan/ricochetRobots/RicochetRobotActivity.java
+++ RicochetRobots-android/src/com/erwan/ricochetRobots/RicochetRobotActivity.java
@@ -1,12 +1,16 @@
 package com.erwan.ricochetRobots;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
@@ -16,13 +20,34 @@ import com.erwan.ricochetRobots.util.RicochetInterface;
 public class RicochetRobotActivity extends AndroidApplication implements
 	RicochetInterface {
 
+    private static final String TAG = "RicochetRobotActivity";
+    private static final boolean D = true;
+    
     private static final int REQUEST_CONNECT_DEVICE = 1;
     private static final int REQUEST_ENABLE_BT = 2;
+
+    // type de message envoyé par le hundle
+    public static final int MESSAGE_STATE_CHANGE = 1;
+    public static final int MESSAGE_READ = 2;
+    public static final int MESSAGE_WRITE = 3;
+    public static final int MESSAGE_DEVICE_NAME = 4;
+    public static final int MESSAGE_TOAST = 5;
+
+    public static final String DEVICE_NAME = "device_name";
+    public static final String TOAST = "toast";
+    
+    // Name of the connected device
+    private String mConnectedDeviceName = null;
+    // Array adapter for the conversation thread
+    private ArrayAdapter<String> mConversationArrayAdapter;
+    // String buffer for outgoing messages
+    private StringBuffer mOutStringBuffer;
 
     private AndroidApplicationConfiguration config;
     private RicochetRobots rb;
 
     private BluetoothAdapter mBluetoothAdapter;
+    private BluetoothRicochet mBluetoothRicochet;
 
     /** Called when the activity is first created. */
     @Override
@@ -115,8 +140,9 @@ public class RicochetRobotActivity extends AndroidApplication implements
 			.getRemoteDevice(address);
 		// Attempt to connect to the device
 
-		// TODO A CONTINUER
-		// mChatService.connect(device);
+		if (mBluetoothRicochet == null)
+		    mBluetoothRicochet = new BluetoothRicochet(this, mHandler);
+		mBluetoothRicochet.connect(device);
 	    }
 	    break;
 	default:
@@ -124,8 +150,60 @@ public class RicochetRobotActivity extends AndroidApplication implements
 	}
     }
 
+    @SuppressLint("HandlerLeak")
+    private final Handler mHandler = new Handler() {
+	public void handleMessage(Message msg) {
+	    switch (msg.what) {
+	    case MESSAGE_STATE_CHANGE:
+		if (D)
+		    Log.i(TAG, "MESSAGE_STATE_CHANGE: " + msg.arg1);
+		switch (msg.arg1) {
+		case BluetoothRicochet.STATE_CONNECTED:
+		   /* mTitle.setText(R.string.title_connected_to);
+		    mTitle.append(mConnectedDeviceName);
+		    mConversationArrayAdapter.clear();*/
+		    break;
+		case BluetoothRicochet.STATE_CONNECTING:
+		   // mTitle.setText(R.string.title_connecting);
+		    break;
+		case BluetoothRicochet.STATE_LISTEN:
+		case BluetoothRicochet.STATE_NONE:
+		   // mTitle.setText(R.string.title_not_connected);
+		    break;
+		}
+		break;
+	    case MESSAGE_WRITE:
+		byte[] writeBuf = (byte[]) msg.obj;
+		// construct a string from the buffer
+		String writeMessage = new String(writeBuf);
+		mConversationArrayAdapter.add("Me: " + writeMessage);
+		break;
+	    case MESSAGE_READ:
+		byte[] readBuf = (byte[]) msg.obj;
+		// construct a string from the valid bytes in the buffer
+		String readMessage = new String(readBuf, 0, msg.arg1);
+		mConversationArrayAdapter.add(mConnectedDeviceName + ": "
+			+ readMessage);
+		break;
+	    case MESSAGE_DEVICE_NAME:
+		// save the connected device's name
+		mConnectedDeviceName = msg.getData().getString(DEVICE_NAME);
+		Toast.makeText(getApplicationContext(),
+			"Connected to " + mConnectedDeviceName,
+			Toast.LENGTH_LONG).show();
+		break;
+	    case MESSAGE_TOAST:
+		Toast.makeText(getApplicationContext(),
+			msg.getData().getString(TOAST), Toast.LENGTH_SHORT)
+			.show();
+		break;
+	    }
+	}
+    };
+
     protected void onDestroy() {
 	super.onDestroy();
 	mBluetoothAdapter.cancelDiscovery();
     }
+
 }
